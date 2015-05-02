3146a3246c062128e6fd246743f429303aa40178
parthoiiitm <partho.iiitm@gmail.com>
2015-05-03 00:30:30 +0530

    First commit

---
+++ obj/local/armeabi/objs-debug/pocketsphinx/acmod.o.d
@@ -0,0 +1,245 @@
+obj/local/armeabi/objs-debug/pocketsphinx/acmod.o: \
+ /home/partho/Desktop/CMUSphinx/pocketsphinx/src/libpocketsphinx/acmod.c \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/assert.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/sys/cdefs.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/android/api-level.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/string.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/malloc.h \
+ /home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/prim_type.h \
+ /home/partho/Desktop/CMUSphinx/sphinxbase/include/android/sphinx_config.h \
+ /home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/err.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/stdio.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/sys/_types.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/machine/_types.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/sys/types.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/stdint.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/linux/posix_types.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/linux/stddef.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/linux/compiler.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/asm/posix_types.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/asm/types.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/linux/types.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/machine/kernel.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/sys/sysmacros.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/errno.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/linux/errno.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/asm/errno.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/asm-generic/errno.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/asm-generic/errno-base.h \
+ /home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/sphinxbase_export.h \
+ /home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/cmd_ln.h \
+ /home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/strfuncs.h \
+ /home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/byteorder.h \
+ /home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/feat.h \
+ /home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/fe.h \
+ /home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/fixpoint.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/limits.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/sys/limits.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/linux/limits.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/machine/internal_types.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/machine/limits.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/sys/syslimits.h \
+ /home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/cmn.h \
+ /home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/agc.h \
+ /home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/bio.h \
+ /home/partho/Desktop/CMUSphinx/pocketsphinx/include/cmdln_macro.h \
+ /home/partho/Desktop/CMUSphinx/pocketsphinx/src/libpocketsphinx/acmod.h \
+ /home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/logmath.h \
+ /home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/bitvec.h \
+ /home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/ckd_alloc.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/stdlib.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/alloca.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/strings.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/memory.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/setjmp.h \
+ /home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/machine/setjmp.h \
+ /home/partho/Desktop/CMUSphinx/pocketsphinx/include/ps_mllr.h \
+ /home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/ngram_model.h \
+ /home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/mmio.h \
+ /home/partho/Desktop/CMUSphinx/pocketsphinx/include/pocketsphinx_export.h \
+ /home/partho/Desktop/CMUSphinx/pocketsphinx/src/libpocketsphinx/bin_mdef.h \
+ /home/partho/Desktop/CMUSphinx/pocketsphinx/src/libpocketsphinx/mdef.h \
+ /home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/hash_table.h \
+ /home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/glist.h \
+ /home/partho/Desktop/CMUSphinx/pocketsphinx/src/libpocketsphinx/tmat.h \
+ /home/partho/Desktop/CMUSphinx/pocketsphinx/src/libpocketsphinx/hmm.h \
+ /home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/listelem_alloc.h \
+ /home/partho/Desktop/CMUSphinx/pocketsphinx/src/libpocketsphinx/s2_semi_mgau.h \
+ /home/partho/Desktop/CMUSphinx/pocketsphinx/src/libpocketsphinx/ms_gauden.h \
+ /home/partho/Desktop/CMUSphinx/pocketsphinx/src/libpocketsphinx/vector.h \
+ /home/partho/Desktop/CMUSphinx/pocketsphinx/src/libpocketsphinx/pocketsphinx_internal.h \
+ /home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/profile.h \
+ /home/partho/Desktop/CMUSphinx/pocketsphinx/include/pocketsphinx.h \
+ /home/partho/Desktop/CMUSphinx/pocketsphinx/include/ps_lattice.h \
+ /home/partho/Desktop/CMUSphinx/pocketsphinx/include/fsg_set.h \
+ /home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/fsg_model.h \
+ /home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/jsgf.h \
+ /home/partho/Desktop/CMUSphinx/pocketsphinx/src/libpocketsphinx/dict.h \
+ /home/partho/Desktop/CMUSphinx/pocketsphinx/src/libpocketsphinx/s3types.h \
+ /home/partho/Desktop/CMUSphinx/pocketsphinx/src/libpocketsphinx/dict2pid.h \
+ /home/partho/Desktop/CMUSphinx/pocketsphinx/src/libpocketsphinx/ptm_mgau.h \
+ /home/partho/Desktop/CMUSphinx/pocketsphinx/src/libpocketsphinx/ms_mgau.h \
+ /home/partho/Desktop/CMUSphinx/pocketsphinx/src/libpocketsphinx/ms_senone.h
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/assert.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/sys/cdefs.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/android/api-level.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/string.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/malloc.h:
+
+/home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/prim_type.h:
+
+/home/partho/Desktop/CMUSphinx/sphinxbase/include/android/sphinx_config.h:
+
+/home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/err.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/stdio.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/sys/_types.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/machine/_types.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/sys/types.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/stdint.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/linux/posix_types.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/linux/stddef.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/linux/compiler.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/asm/posix_types.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/asm/types.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/linux/types.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/machine/kernel.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/errno.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/linux/errno.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/asm/errno.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/asm-generic/errno.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/asm-generic/errno-base.h:
+
+/home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/sphinxbase_export.h:
+
+/home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/cmd_ln.h:
+
+/home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/strfuncs.h:
+
+/home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/byteorder.h:
+
+/home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/feat.h:
+
+/home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/fe.h:
+
+/home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/fixpoint.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/limits.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/sys/limits.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/linux/limits.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/machine/internal_types.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/machine/limits.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/sys/syslimits.h:
+
+/home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/cmn.h:
+
+/home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/agc.h:
+
+/home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/bio.h:
+
+/home/partho/Desktop/CMUSphinx/pocketsphinx/include/cmdln_macro.h:
+
+/home/partho/Desktop/CMUSphinx/pocketsphinx/src/libpocketsphinx/acmod.h:
+
+/home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/logmath.h:
+
+/home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/bitvec.h:
+
+/home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/ckd_alloc.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/stdlib.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/alloca.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/strings.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/memory.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/setjmp.h:
+
+/home/partho/android-ndk-r9d/platforms/android-16/arch-arm/usr/include/machine/setjmp.h:
+
+/home/partho/Desktop/CMUSphinx/pocketsphinx/include/ps_mllr.h:
+
+/home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/ngram_model.h:
+
+/home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/mmio.h:
+
+/home/partho/Desktop/CMUSphinx/pocketsphinx/include/pocketsphinx_export.h:
+
+/home/partho/Desktop/CMUSphinx/pocketsphinx/src/libpocketsphinx/bin_mdef.h:
+
+/home/partho/Desktop/CMUSphinx/pocketsphinx/src/libpocketsphinx/mdef.h:
+
+/home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/hash_table.h:
+
+/home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/glist.h:
+
+/home/partho/Desktop/CMUSphinx/pocketsphinx/src/libpocketsphinx/tmat.h:
+
+/home/partho/Desktop/CMUSphinx/pocketsphinx/src/libpocketsphinx/hmm.h:
+
+/home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/listelem_alloc.h:
+
+/home/partho/Desktop/CMUSphinx/pocketsphinx/src/libpocketsphinx/s2_semi_mgau.h:
+
+/home/partho/Desktop/CMUSphinx/pocketsphinx/src/libpocketsphinx/ms_gauden.h:
+
+/home/partho/Desktop/CMUSphinx/pocketsphinx/src/libpocketsphinx/vector.h:
+
+/home/partho/Desktop/CMUSphinx/pocketsphinx/src/libpocketsphinx/pocketsphinx_internal.h:
+
+/home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/profile.h:
+
+/home/partho/Desktop/CMUSphinx/pocketsphinx/include/pocketsphinx.h:
+
+/home/partho/Desktop/CMUSphinx/pocketsphinx/include/ps_lattice.h:
+
+/home/partho/Desktop/CMUSphinx/pocketsphinx/include/fsg_set.h:
+
+/home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/fsg_model.h:
+
+/home/partho/Desktop/CMUSphinx/sphinxbase/include/sphinxbase/jsgf.h:
+
+/home/partho/Desktop/CMUSphinx/pocketsphinx/src/libpocketsphinx/dict.h:
+
+/home/partho/Desktop/CMUSphinx/pocketsphinx/src/libpocketsphinx/s3types.h:
+
+/home/partho/Desktop/CMUSphinx/pocketsphinx/src/libpocketsphinx/dict2pid.h:
+
+/home/partho/Desktop/CMUSphinx/pocketsphinx/src/libpocketsphinx/ptm_mgau.h:
+
+/home/partho/Desktop/CMUSphinx/pocketsphinx/src/libpocketsphinx/ms_mgau.h:
+
+/home/partho/Desktop/CMUSphinx/pocketsphinx/src/libpocketsphinx/ms_senone.h:
---
+++ src/edu/cmu/pocketsphinx/demo/PocketSphinxAndroidDemo.java
@@ -0,0 +1,241 @@
+package edu.cmu.pocketsphinx.demo;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Date;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.res.AssetManager;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class PocketSphinxAndroidDemo extends Activity implements OnTouchListener, RecognitionListener {
+	static {
+		System.loadLibrary("pocketsphinx_jni");
+	}
+	/**
+	 * Recognizer task, which runs in a worker thread.
+	 */
+	RecognizerTask rec;
+	/**
+	 * Thread in which the recognizer task runs.
+	 */
+	Thread rec_thread;
+	/**
+	 * Time at which current recognition started.
+	 */
+	Date start_date;
+	/**
+	 * Number of seconds of speech.
+	 */
+	float speech_dur;
+	/**
+	 * Are we listening?
+	 */
+	boolean listening;
+	/**
+	 * Progress dialog for final recognition.
+	 */
+	ProgressDialog rec_dialog;
+	/**
+	 * Performance counter view.
+	 */
+	TextView performance_text;
+	/**
+	 * Editable text view.
+	 */
+	EditText edit_text;
+	
+	Button buttonSend;
+	EditText textPhoneNo;
+	
+	
+	/**
+	 * Respond to touch events on the Speak button.
+	 * 
+	 * This allows the Speak button to function as a "push and hold" button, by
+	 * triggering the start of recognition when it is first pushed, and the end
+	 * of recognition when it is released.
+	 * 
+	 * @param v
+	 *            View on which this event is called
+	 * @param event
+	 *            Event that was triggered.
+	 */
+	public boolean onTouch(View v, MotionEvent event) {
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			start_date = new Date();
+			this.listening = true;
+			this.rec.start();
+			break;
+		case MotionEvent.ACTION_UP:
+			Date end_date = new Date();
+			long nmsec = end_date.getTime() - start_date.getTime();
+			this.speech_dur = (float)nmsec / 1000;
+			if (this.listening) {
+				Log.d(getClass().getName(), "Showing Dialog");
+				this.rec_dialog = ProgressDialog.show(PocketSphinxAndroidDemo.this, "", "Recognizing speech...", true);
+				this.rec_dialog.setCancelable(false);
+				this.listening = false;
+			}
+			this.rec.stop();
+			break;
+		default:
+			;
+		}
+		/* Let the button handle its own state */
+		return false;
+	}
+
+	/** Called when the activity is first created. */
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		copyAsset("pocketsphinxmodels");
+		this.rec = new RecognizerTask(new File(getExternalFilesDir(null), "pocketsphinxmodels"));
+		this.rec_thread = new Thread(this.rec);
+		this.listening = false;
+		Button b = (Button) findViewById(R.id.Button01);
+		b.setOnTouchListener(this);
+		this.performance_text = (TextView) findViewById(R.id.PerformanceText);
+		this.edit_text = (EditText) findViewById(R.id.EditText01);
+		this.rec.setRecognitionListener(this);
+		this.rec_thread.start();
+		buttonSend = (Button) findViewById(R.id.buttonSend);
+		textPhoneNo = (EditText) findViewById(R.id.editTextPhoneNo);
+		buttonSend.setOnClickListener(new OnClickListener() {
+			 
+			@Override
+			public void onClick(View v) {
+ 
+			  String phoneNo = textPhoneNo.getText().toString();
+			  String sms = edit_text.getText().toString();
+ 
+			  try {
+				SmsManager smsManager = SmsManager.getDefault();
+				smsManager.sendTextMessage(phoneNo, null, sms, null, null);
+				Toast.makeText(getApplicationContext(), "SMS Sent!",
+							Toast.LENGTH_LONG).show();
+			  } catch (Exception e) {
+				Toast.makeText(getApplicationContext(),
+					"SMS failed, please try again later!",
+					Toast.LENGTH_LONG).show();
+				e.printStackTrace();
+			  }
+ 
+			}
+		});
+	}
+	
+	public void copyAsset(String path) {
+		  AssetManager manager = getAssets();
+		  
+		  File f = new File(getExternalFilesDir(null), path);
+		  if(!f.isDirectory()){
+
+			  // If we have a directory, we make it and recurse. If a file, we copy its
+			  // contents.
+			  try {
+			    String[] contents = manager.list(path);
+	
+			    // The documentation suggests that list throws an IOException, but doesn't
+			    // say under what conditions. It'd be nice if it did so when the path was
+			    // to a file. That doesn't appear to be the case. If the returned array is
+			    // null or has 0 length, we assume the path is to a file. This means empty
+			    // directories will get turned into files.
+			    if (contents == null || contents.length == 0)
+			      throw new IOException();
+	
+			    // Make the directory.
+			    File dir = new File(getExternalFilesDir(null), path);
+			    dir.mkdirs();
+	
+			    // Recurse on the contents.
+			    for (String entry : contents) {
+			      copyAsset(path + "/" + entry);
+			    }
+			  } catch (IOException e) {
+			    copyFileAsset(path);
+			  }
+		  }
+		}
+
+		/**
+		 * Copy the asset file specified by path to app's data directory. Assumes
+		 * parent directories have already been created.
+		 * 
+		 * @param path
+		 * Path to asset, relative to app's assets directory.
+		 */
+		public void copyFileAsset(String path) {
+		  File file = new File(getExternalFilesDir(null), path);
+		  try {
+		    InputStream in = getAssets().open(path);
+		    OutputStream out = new FileOutputStream(file);
+		    byte[] buffer = new byte[1024];
+		    int read = in.read(buffer);
+		    while (read != -1) {
+		      out.write(buffer, 0, read);
+		      read = in.read(buffer);
+		    }
+		    out.close();
+		    in.close();
+		  } catch (IOException e) {
+		    //Log.d(path, path, e);
+		  }
+		}
+	
+
+	/** Called when partial results are generated. */
+	public void onPartialResults(Bundle b) {
+		final PocketSphinxAndroidDemo that = this;
+		final String hyp = b.getString("hyp");
+		that.edit_text.post(new Runnable() {
+			public void run() {
+				that.edit_text.setText(hyp);
+			}
+		});
+	}
+
+	/** Called with full results are generated. */
+	public void onResults(Bundle b) {
+		final String hyp = b.getString("hyp");
+		final PocketSphinxAndroidDemo that = this;
+		this.edit_text.post(new Runnable() {
+			public void run() {
+				that.edit_text.setText(hyp);
+				Date end_date = new Date();
+				long nmsec = end_date.getTime() - that.start_date.getTime();
+				float rec_dur = (float)nmsec / 1000;
+				that.performance_text.setText(String.format("%.2f seconds %.2f xRT",
+															that.speech_dur,
+															rec_dur / that.speech_dur));
+				Log.d(getClass().getName(), "Hiding Dialog");
+				that.rec_dialog.dismiss();
+			}
+		});
+	}
+
+	public void onError(int err) {
+		final PocketSphinxAndroidDemo that = this;
+		that.edit_text.post(new Runnable() {
+			public void run() {
+				that.rec_dialog.dismiss();
+			}
+		});
+	}
+}
